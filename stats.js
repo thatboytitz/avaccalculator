ad181f0a99db9c573fba58da4af0cbf203516301
thatboytitz <thatboytitz@users.noreply.github.com>
2017-06-04 13:39:22 -0700

    added even more spaces
    " " SPACES " "

--- stats.js
+++ stats.js
@@ -3,27 +3,27 @@ var heroAtk = [];
 heroAtk["Bucky"] =					[500,560,620,740,860,1040,1220];
 heroAtk["Captain America 2099"] =				[450,505,560,670,780,945,1110];
 heroAtk["Loki"] =					[100,150,200,300,400,550,700];
-heroAtk["MedievalSpider Woman"] =			[650,715,780,910,1040,1235,1430];
+heroAtk["Medieval Spider Woman"] =			[650,715,780,910,1040,1235,1430];
 heroAtk["Wasp"] = 					[100,150,200,300,400,550,700];
-heroAtk["OriginalWasp"] =				[300,350,400,500,600,750,900];
+heroAtk["Original Wasp"] =				[300,350,400,500,600,750,900];
 
 
 var heroHP = [];
 heroHP["Bucky"] = 					[1500,1680,1860,2220,2580,3120,3660];
-heroHP["Captain America2099"] =				[1350,1515,1680,2010,2340,2835,3330];
+heroHP["Captain America 2099"] =				[1350,1515,1680,2010,2340,2835,3330];
 heroHP["Loki"] = 					[300,450,600,900,1200,1650,2100];
-heroHP["MedievalSpider Woman"] =				[1950,2145,2340,2730,3120,3705,4290];
+heroHP["Medieval Spider Woman"] =				[1950,2145,2340,2730,3120,3705,4290];
 heroHP["Wasp"] =					[300,450,600,900,1200,1650,2100];
-heroHP["OriginalWasp"] =				[900,1050,1200,1500,1800,2250,2700];
+heroHP["Original Wasp"] =				[900,1050,1200,1500,1800,2250,2700];
 
 var heroType = [];
 // Poison: 1, Quick: 2, Strong: 3, AdvPoison: 4, AdvQuick: 5, AdvStrong: 6, Ult: 7
 heroType["Bucky"] 						= 1;
 heroType["Captain America 2099"] 			= 3;
 heroType["Loki"] 						= 1;
-heroType["MedievalSpider Woman"]			= 2;
+heroType["Medieval Spider Woman"]			= 2;
 heroType["Wasp"]						= 2;
-heroType["OriginalWasp"] 				= 2;
+heroType["Original Wasp"] 				= 2;
 
 
 var quadrantHP = [
