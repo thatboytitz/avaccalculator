d92374da332811b5df4ee6021c503014bd7edbe3
thatboytitz <mmejia1337@gmail.com>
2017-06-08 12:54:58 -0700

    bugs and fixes
    changed loki's display to the right type and added complete stat chart

--- stats.js
+++ stats.js
@@ -1,33 +1,33 @@
-var heroHP = [];
-
-heroHP["Bucky"] =							[500,560,620,740,0,0,0,0,0,0];
-heroHP["CaptainAmerica2099"] =				[0,0,0,0,0,0,0,0,0,0];
-heroHP["Loki"] =							[100,150,200,300,0,0,0,0,0,0];
-heroHP["MedievalSpiderWoman"] =				[650,0,0,0,0,0,0,0,0,0];
-heroHP["Wasp"] = 							[100,150,200,300,0,0,0,0,0,0];
-heroHP["OriginalWasp"] =					[0,0,0,0,0,0,0,0,0,0];
-
-
 var heroAtk = [];
-heroAtk["Bucky"] = 							[1500,1680,1860,2220,0,0,0,0,0,0];
-heroAtk["CaptainAmerica2099"] =				[0,0,0,0,0,0,0,0,0,0];
-heroAtk["Loki"] = 							[300,450,0,0,0,0,0,0,0,0];
-heroAtk["MedievalSpiderWoman"] =			[1950,0,0,0,0,0,0,0,0,0];
-heroAtk["Wasp"] =							[300,450,0,0,0,0,0,0,0,0];
-heroAtk["OriginalWasp"] =					[0,0,0,0,0,0,0,0,0,0];
+
+heroAtk["Bucky"] =								[500,560,620,740,860,1040,1220,0,0,0];
+heroAtk["Captain America 2099"] =				[450,505,560,670,780,945,1100,0,0,0];
+heroAtk["Loki"] =								[100,150,200,300,400,550,700,0,0,0];
+heroAtk["Medieval Spider Woman"] =				[650,715,780,910,1040,1235,1430,0,0,0];
+heroAtk["Loki"] =								[100,150,200,300,400,550,700,0,0,0];
+heroAtk["Original Wasp"] =						[300,350,400,500,600,750,900,0,0,0];
+
+
+var heroHP = [];
+heroHP["Bucky"] = 								[1500,1680,1860,2220,2580,3120,3660,0,0,0];
+heroHP["Captain America 2099"] =				[1350,1515,1680,2010,2340,2835,3330,0,0,0];
+heroHP["Loki"] = 								[300,450,600,900,1200,1650,2100,0,0,0];
+heroHP["Medieval Spider Woman"] =				[1950,2145,2340,2730,3120,3705,4290,0,0,0];
+heroHP["Wasp"] = 								[300,450,600,900,1200,1650,2100,0,0,0];
+heroHP["Original Wasp"] =						[900,1050,1200,1500,1800,2250,2700,0,0,0];
 
 var heroType = [];
 // Poison: 1, Quick: 2, Strong: 3, AdvPoison: 4, AdvQuick: 5, AdvStrong: 6, Ult: 7
-heroType["Bucky"] 						= 1;
-heroType["CaptainAmerica2099"] 			= 3;
-heroType["Loki"] 						= 1;
-heroType["MedievalSpiderWoman"]			= 2;
-heroType["Wasp"]						= 2;
-heroType["OriginalWasp"] 				= 2;
+heroType["Bucky"] 							= 1;
+heroType["Captain America 2099"] 			= 3;
+heroType["Loki"] 							= 1;
+heroType["Medieval Spider Woman"]			= 1;
+heroType["Wasp"]							= 2;
+heroType["Original Wasp"] 					= 2;
 
 
 var quadrantHP = [
-	[720,960,1200,1680,2400,0,0,0],
+	[720,960,1200,1680,2400,3360,4200,5520],
 	[0,0,0,0,0,0,0,0],
 	[0,0,0,0,0,0,0,0],
 	[0,0,0,0,0,0,0,0],
@@ -35,7 +35,7 @@ var quadrantHP = [
 ];
 
 var quadrantAtk = [
-	[120,240,240,360,430,0,0,0],
+	[120,150,210,280,450,580,780,1050],
 	[0,0,0,0,0,0,0,0],
 	[0,0,0,0,0,0,0,0],
 	[0,0,0,0,0,0,0,0],
@@ -44,7 +44,7 @@ var quadrantAtk = [
 
 var quadrantType = [
 // Poison: 1, Quick: 2, Strong: 3, AdvPoison: 4, AdvQuick: 5, AdvStrong: 6, Ult: 7
-	[2,2,2,2,3,0,0,0],
+	[2,2,2,2,3,3,3,1],
 	[0,0,0,0,0,0,0,0],
 	[0,0,0,0,0,0,0,0],
 	[0,0,0,0,0,0,0,0],
@@ -53,23 +53,23 @@ var quadrantType = [
 
 
 var heroicQuadrantHP = [
-	[0,0,0,0,0,0],
-	[0,0,0,0,0,0],
-	[0,0,0,0,0,0],
-	[0,0,0,0,0,0]
+	[3120,3690,4800,5520,6000,0,0,0],
+	[0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0]
 ];
 
 var heroicQuadrantAtk = [
-	[0,0,0,0,0,0],
-	[0,0,0,0,0,0],
-	[0,0,0,0,0,0],
-	[0,0,0,0,0,0]
+	[560,750,800,1080,1200,0,0,0],
+	[0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0]
 ];
 
 var heroicQuadrantType = [
 // Poison: 1, Quick: 2, Strong: 3, AdvPoison: 4, AdvQuick: 5, AdvStrong: 6, Ult: 7
-	[0,0,0,0,0,0],
-	[0,0,0,0,0,0],
-	[0,0,0,0,0,0],
-	[0,0,0,0,0,0]
+	[1,2,3,1,2,0,0,0],
+	[0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0]
 ];
